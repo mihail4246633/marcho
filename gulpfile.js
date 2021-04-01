c6d3be54976c3d02018599464ecd2df4d9832932
mihail <mshestak4246633@gmail.com>
2021-04-01 18:40:57 +0300

    added top-slider section

--- gulpfile.js
+++ gulpfile.js
@@ -30,6 +30,7 @@ const styles = () => {
 const scripts = () => {
     return src([
             'node_modules/jquery/dist/jquery.js',
+            'node_modules/slick-carousel/slick/slick.js',
             'app/js/main.js'
         ])
         .pipe(concat('main.min.js'))
