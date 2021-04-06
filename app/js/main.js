fab910ca2404dca66b085078476b90cbce094d76
mihail <mshestak4246633@gmail.com>
2021-04-06 19:08:14 +0300

    added section products and star-rating

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,17 @@
 $(function() {
+
     $('.top-slider__inner').slick({
         dots: true,
         arrows: false,
         fade: true,
         autoplay: true
     });
+
+    $(".star").rateYo({
+        starWidth: "17px",
+        normalFill: "#ccccce",
+        ratedFill: "#ffc35b",
+        readOnly: true
+    });
+
 })
