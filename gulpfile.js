c1e6b7a831c82b4f7ffd54f6b39b95f4e36f5ea6
mihail <mshestak4246633@gmail.com>
2021-04-19 18:54:27 +0300

    added login page

--- gulpfile.js
+++ gulpfile.js
@@ -75,4 +75,4 @@ exports.watching = watching;
 exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, images, build);
 
-exports.default = parallel(styles, scripts, browsersync, watching)
+exports.default = parallel(styles, scripts, browsersync, watching);
